f73159cb92128590e27c406a79114ee3779dc817
omarelalami <omaralami230@gmail.com>
2023-12-17 15:49:06 +0100

    prétraitement de données pour OCR

--- Pygame/main.py
+++ Pygame/main.py
@@ -3,16 +3,27 @@ import pygame
 WIDTH,HEIGHT= 900,500
 WIN=pygame.display.set_mode((WIDTH,HEIGHT))
 WHITE=(255,255,255)
+FPS=60
+YELLOW_SPACESHIP_IMAGE=pygame.image.load('img.png')
+
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP_IMAGE,(300,100))
+    pygame.display.update()
+
+
 def main():
+    clock=pygame.time.Clock()
     run=True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
-                run=True
+                run=False
+        draw_window()
         
-        WIN.fill(WHITE)
-        pygame.display.update()
     pygame.quit()
 
-if __name__=="__main__":
+if __name__=="__main__":    
     main()
