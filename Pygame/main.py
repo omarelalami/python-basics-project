a69fc27efc4e5b1fdf97c345e52f1b9283f34688
omarelalami <omaralami230@gmail.com>
2023-12-17 14:45:13 +0100

    init pygame env

---
+++ Pygame/main.py
@@ -0,0 +1,18 @@
+import pygame
+
+WIDTH,HEIGHT= 900,500
+WIN=pygame.display.set_mode((WIDTH,HEIGHT))
+WHITE=(255,255,255)
+def main():
+    run=True
+    while run:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=True
+        
+        WIN.fill(WHITE)
+        pygame.display.update()
+    pygame.quit()
+
+if __name__=="__main__":
+    main()
