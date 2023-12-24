841979d6fc18ebe2ae2f09ab09cc2bba997fda8e
omarelalami <omaralami230@gmail.com>
2023-12-24 15:41:31 +0100

    begin to fight game

--- Pygame/main.py
+++ Pygame/main.py
@@ -8,6 +8,7 @@ pygame.display.set_caption("Moroccan Simle Game")
 BORDER=pygame.Rect(WIDTH/2 - 5,0,10,HEIGHT)
 SPACESHIP_WIDTH,SPACESHIP_HEIGHT=100,70
 FPS=90
+BULLET_VEL = 7
 YELLOW_SPACESHIP_IMAGE=pygame.image.load('moro.png')
 RED_SPACESHIP_IMAGE=pygame.image.load('moroo.png')
 YELLOW_SPACESHIP=pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
@@ -25,18 +26,19 @@ def draw_window(red,yellow):
 
 def yellow_handle_movement(keys_pressed,yellow):
        
-    if keys_pressed[pygame.K_LEFT] and yellow.x - 5 > 0:
+    if keys_pressed[pygame.K_LEFT] and yellow.x - 5  > BORDER.x + BORDER.width:
         yellow.x-=5
-    if keys_pressed[pygame.K_RIGHT] and yellow.x + 5 + yellow.width < BORDER.x:
+    if keys_pressed[pygame.K_RIGHT] and yellow.x + 5 + yellow.width < WIDTH:
         yellow.x+=5
     if keys_pressed[pygame.K_UP] and yellow.y - 5 > 0:
         yellow.y-=5
     if keys_pressed[pygame.K_DOWN] and yellow.y - 5 < HEIGHT - 70:
         yellow.y+=5
+
 def red_handle_movement(keys_pressed,red):
     if keys_pressed[pygame.K_a] and red.x - 5 > 0:
         red.x-=5
-    if keys_pressed[pygame.K_b] and red.x - 5 < WIDTH - 100:
+    if keys_pressed[pygame.K_b] and red.x - 5 + red.width < BORDER.x:
         red.x+=5
     if keys_pressed[pygame.K_s] and red.y - 5 > 0:
         red.y-=5
@@ -50,6 +52,9 @@ def red_handle_movement(keys_pressed,red):
 def main():
     red=pygame.Rect(100,300,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
     yellow=pygame.Rect(700,300,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
+    
+    bullets=[]
+
     clock=pygame.time.Clock()
     run=True
     while run:
